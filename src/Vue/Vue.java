11c922f72699a1feaab24b253b3563f85ddea212
charlottetrupin <59920560+charlottetrupin@users.noreply.github.com>
2021-05-07 10:45:40 +0200

    Add files via upload

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -5,11 +5,14 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * Class pour lancer la page d'acceuil
+ */
 public class Vue extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane start = (Pane) FXMLLoader.load(getClass().getResource("../Control/acceuil.fxml"));
+        Pane start = (Pane) FXMLLoader.load(getClass().getResource("../Fxml/acceuil.fxml"));
         Scene welcome = new Scene(start,start.getPrefWidth(),start.getPrefHeight());
         primaryStage.setResizable(false);
         primaryStage.setTitle("Cuisine2D");
